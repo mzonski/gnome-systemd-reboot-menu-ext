bf2f5f9e75e90276c3c9f1de2f87b6db84c79f8e
Maciej Zonski <me@zonni.pl>
2025-08-09 23:46:44 +0200

    refactor extension

--- src/extension.ts
+++ src/extension.ts
@@ -36,194 +36,325 @@ import {
 import type { QuickToggleMenu } from '@girs/gnome-shell/ui/quickSettings';
 import { ExtensionMetadata } from '@girs/gnome-shell/extensions/extension';
 
+const REBOOT_COUNTDOWN_SECONDS = 10;
+const COUNTDOWN_UPDATE_INTERVAL = 1;
+const MESSAGE_UPDATE_INTERVAL_MS = 500;
+
+const BOOT_TARGETS = {
+  BIOS: 'auto-reboot-to-firmware-setup',
+  WINDOWS: 'windows-11',
+} as const;
+
+
+class Timer {
+  private _sourceId: number | null = null;
+
+  start(callback: () => boolean, intervalMs: number, priority: number = GLib.PRIORITY_DEFAULT) {
+    this.stop();
+    this._sourceId = GLib.timeout_add(priority, intervalMs, callback);
+  }
+
+  startSeconds(callback: () => boolean, intervalSeconds: number, priority: number = GLib.PRIORITY_DEFAULT) {
+    this.stop();
+    this._sourceId = GLib.timeout_add_seconds(priority, intervalSeconds, callback);
+  }
+
+  stop() {
+    if (this._sourceId) {
+      GLib.Source.remove(this._sourceId);
+      this._sourceId = null;
+    }
+  }
+}
+
+class RebootCountdown {
+  private remainingSeconds: number;
+  private displaySeconds: number;
+  private readonly _countdownTimer = new Timer();
+  private readonly _displayTimer = new Timer();
+  private readonly _onComplete: () => void;
+  private readonly _onUpdate: (seconds: number) => void;
+
+  constructor(onComplete: () => void, onUpdate: (seconds: number) => void) {
+    this.remainingSeconds = REBOOT_COUNTDOWN_SECONDS;
+    this.displaySeconds = this.remainingSeconds;
+    this._onComplete = onComplete;
+    this._onUpdate = onUpdate;
+  }
+
+  start() {
+    this.remainingSeconds = REBOOT_COUNTDOWN_SECONDS;
+    this.displaySeconds = this.remainingSeconds;
+    this._onUpdate(this.displaySeconds);
+
+    this._countdownTimer.startSeconds(() => {
+      this.remainingSeconds--;
+
+      if (this.remainingSeconds % COUNTDOWN_UPDATE_INTERVAL === 0) {
+        this.displaySeconds = this.remainingSeconds;
+      }
+
+      if (this.remainingSeconds <= 0) {
+        this.stop();
+        this._onComplete();
+        return GLib.SOURCE_REMOVE;
+      }
+
+      return GLib.SOURCE_CONTINUE;
+    }, 1);
+
+    this._displayTimer.start(() => {
+      this._onUpdate(this.displaySeconds);
+      return GLib.SOURCE_CONTINUE;
+    }, MESSAGE_UPDATE_INTERVAL_MS);
+  }
+
+  stop() {
+    this._countdownTimer.stop();
+    this._displayTimer.stop();
+  }
+}
+
+class RebootConfirmationDialog {
+  private readonly messageLabel: St.Label;
+  private dialog: ModalDialog.ModalDialog;
+  private countdown: RebootCountdown;
+
+  constructor(title: string, onConfirm: () => void, onCancel: () => void) {
+    this.dialog = this.createDialog(title, onConfirm, onCancel);
+    this.messageLabel = this.createMessageLabel();
+    this.countdown = new RebootCountdown(
+      onConfirm,
+      (seconds) => this.updateMessage(seconds),
+    );
+
+    this.setupDialogContent(title);
+  }
+
+  private createDialog(title: string, onConfirm: () => void, onCancel: () => void): ModalDialog.ModalDialog {
+    const dialog = new ModalDialog.ModalDialog({ styleClass: 'modal-dialog' });
+
+    dialog.setButtons([
+      {
+        label: _('Cancel'),
+        action: () => {
+          this.destroy();
+          onCancel();
+        },
+        key: Clutter.KEY_Escape,
+        default: false,
+      },
+      {
+        label: _('Restart Now'),
+        action: () => {
+          this.destroy();
+          onConfirm();
+        },
+        default: false,
+      },
+    ]);
+
+    return dialog;
+  }
+
+  private createMessageLabel(): St.Label {
+    const label = new St.Label({
+      text: this.getMessageText(REBOOT_COUNTDOWN_SECONDS),
+    });
+    label.clutterText.ellipsize = Pango.EllipsizeMode.NONE;
+    label.clutterText.lineWrap = true;
+    return label;
+  }
+
+  private setupDialogContent(title: string) {
+    const titleLabel = new St.Label({
+      text: _(title),
+      style: 'font-weight: bold; font-size: 18px; margin-bottom: 12px;',
+    });
+
+    const titleBox = new St.BoxLayout({
+      xAlign: Clutter.ActorAlign.CENTER,
+    });
+    titleBox.add_child(titleLabel);
+
+    const contentBox = new St.BoxLayout({
+      yExpand: true,
+      vertical: true,
+      style: 'spacing: 12px;',
+    });
+    contentBox.add_child(titleBox);
+    contentBox.add_child(this.messageLabel);
+
+    this.dialog.contentLayout.add_child(contentBox);
+  }
+
+  private updateMessage(seconds: number) {
+    this.messageLabel.set_text(this.getMessageText(seconds));
+  }
+
+  private getMessageText(seconds: number) {
+    return _('The system will restart automatically in %d seconds.').replace('%d', String(seconds));
+  }
+
+  show() {
+    this.dialog.open();
+    this.countdown.start();
+  }
+
+  destroy() {
+    this.countdown.stop();
+    this.dialog.close();
+  }
+}
+
 const RebootMenuItem = GObject.registerClass(
   class RebootMenuItem extends PopupMenu.PopupMenuItem {
-    private counter!: number;
-    private seconds!: number;
-    private counterIntervalId!: number | null;
-    private messageIntervalId!: number | null;
+    private dialog: RebootConfirmationDialog | null = null;
 
-    constructor(title: string, onReboot: () => void) {
+    constructor(private title: string, private onReboot: () => void) {
       super(`${_(title)}...`);
-
-      this.counter = 0;
-      this.seconds = 0;
-      this.counterIntervalId = null;
-      this.messageIntervalId = null;
-
-      this.connect('activate', () => {
-        this.counter = 60;
-        this.seconds = this.counter;
-
-        const dialog = this.buildDialog(title, onReboot);
-        dialog.open();
-
-        this.counterIntervalId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 1, () => {
-          if (this.counter > 0) {
-            this.counter--;
-            if (this.counter % 10 === 0) {
-              this.seconds = this.counter;
-            }
-            return GLib.SOURCE_CONTINUE;
-          } else {
-            this.clearIntervals();
-            onReboot();
-            return GLib.SOURCE_REMOVE;
-          }
-        });
-      });
+      this.connect('activate', () => this._handleActivation());
     }
 
     destroy() {
-      this.clearIntervals();
+      this._cleanup();
       super.destroy();
     }
 
-    private buildDialog(title: string, onSkipTimer: () => void): ModalDialog.ModalDialog {
-      const dialog = new ModalDialog.ModalDialog({ styleClass: 'modal-dialog' });
-      dialog.setButtons([
-        {
-          label: _('Cancel'),
-          action: () => {
-            this.clearIntervals();
-            dialog.close();
-          },
-          key: Clutter.KEY_Escape,
-          default: false,
-        },
-        {
-          label: _('Restart'),
-          action: () => {
-            this.clearIntervals();
-            onSkipTimer();
-          },
-          default: false,
-        },
-      ]);
+    _handleActivation() {
+      if (this.dialog) {
+        this.dialog.destroy();
+      }
 
-      const dialogTitle = new St.Label({
-        text: _(title),
-        style: 'font-weight: bold;font-size:18px',
-      });
-
-      let dialogMessage = new St.Label({
-        text: this.getDialogMessageText(),
-      });
-      dialogMessage.clutterText.ellipsize = Pango.EllipsizeMode.NONE;
-      dialogMessage.clutterText.lineWrap = true;
-
-      const titleBox = new St.BoxLayout({
-        xAlign: Clutter.ActorAlign.CENTER,
-      });
-      titleBox.add_child(new St.Label({ text: '  ' }));
-      titleBox.add_child(dialogTitle);
-
-      let box = new St.BoxLayout({ yExpand: true, vertical: true });
-      box.add_child(titleBox);
-      box.add_child(new St.Label({ text: '  ' }));
-      box.add_child(dialogMessage);
-
-      this.messageIntervalId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 500, () => {
-        dialogMessage?.set_text(this.getDialogMessageText());
-        return GLib.SOURCE_CONTINUE;
-      });
-
-      dialog.contentLayout.add_child(box);
-
-      return dialog;
-    }
-
-    private getDialogMessageText(): string {
-      return _(`The system will restart automatically in %d seconds.`).replace(
-        '%d',
-        String(this.seconds),
+      this.dialog = new RebootConfirmationDialog(
+        this.title,
+        () => this._executeReboot(),
+        () => this._cancelReboot(),
       );
+
+      this.dialog.show();
     }
 
-    private clearIntervals(): void {
-      if (this.counterIntervalId) {
-        GLib.Source.remove(this.counterIntervalId);
-        this.counterIntervalId = null;
-      }
-      if (this.messageIntervalId) {
-        GLib.Source.remove(this.messageIntervalId);
-        this.messageIntervalId = null;
+    _executeReboot() {
+      this._cleanup();
+      this.onReboot();
+    }
+
+    _cancelReboot() {
+      this._cleanup();
+    }
+
+    _cleanup() {
+      if (this.dialog) {
+        this.dialog = null;
       }
     }
-  });
+  },
+);
 
-const ManagerInterface = `<node>
-  <interface name="org.freedesktop.login1.Manager">
-    <method name="Reboot">
-      <arg type="b" direction="in"/>
-    </method>
-  </interface>
-  </node>` as string;
+class SystemRebootManager {
+  async reboot(bootTarget: string) {
+    try {
+      await this.setBootTarget(bootTarget);
+    } catch (error) {
+      log('Failed to execute reboot: ' + error);
+      throw error;
+    }
+  }
+
+  private async setBootTarget(target: string) {
+    const command = `pkexec systemctl reboot --boot-loader-entry=${target}`;
+    const [, argv] = GLib.shell_parse_argv(command);
+    const process = Gio.Subprocess.new(argv!, Gio.SubprocessFlags.NONE);
+    await process.wait_check_async(null);
+  }
+}
+
+class SystemMenuIntegration {
+  private menu: QuickToggleMenu | null = null;
+  private menuItems: InstanceType<typeof RebootMenuItem>[] = [];
+  private initializationTimer = new Timer();
+
+  constructor(private rebootManager: SystemRebootManager) {
+  }
+
+  initialize() {
+    if (panel.statusArea.quickSettings._system) {
+      this._setupMenuItems();
+    } else {
+      this._waitForSystemMenu();
+    }
+  }
+
+  destroy() {
+    this.initializationTimer.stop();
+    this.menuItems.forEach(item => item.destroy());
+    this.menuItems = [];
+    this.menu = null;
+  }
+
+  _waitForSystemMenu() {
+    this.initializationTimer.start(() => {
+      if (panel.statusArea.quickSettings._system) {
+        this._setupMenuItems();
+        return GLib.SOURCE_REMOVE;
+      }
+      return GLib.SOURCE_CONTINUE;
+    }, 100);
+  }
+
+  _setupMenuItems() {
+    this.menu = panel.statusArea.quickSettings._system?.quickSettingsItems[0].menu;
+
+    if (!this.menu) {
+      log('Failed to access system menu');
+      return;
+    }
+
+    const biosRebootItem = new RebootMenuItem(
+      'Restart to BIOS',
+      () => this.rebootManager.reboot(BOOT_TARGETS.BIOS),
+    );
+
+    const windowsRebootItem = new RebootMenuItem(
+      'Restart to Windows',
+      () => this.rebootManager.reboot(BOOT_TARGETS.WINDOWS),
+    );
+
+    this.menu.addMenuItem(windowsRebootItem, 2);
+    this.menu.addMenuItem(biosRebootItem, 3);
+
+    this.menuItems = [biosRebootItem, windowsRebootItem];
+  }
+}
 
 export default class SystemDRebootMenuExtended extends Extension {
-  private menu: QuickToggleMenu | null = null;
-  private proxy: Gio.DBusProxy | null = null;
-  private rebootToBiosPowerMenuItem: PopupMenu.PopupMenuItem | null = null;
-  private rebootToWindowsPowerMenuItem: PopupMenu.PopupMenuItem | null = null;
-  private sourceId: number | null = null;
-  private manager: ReturnType<typeof Gio.DBusProxy.makeProxyWrapper<typeof ManagerInterface>> | null = null;
+  private rebootManager: SystemRebootManager | null = null;
+  private menuIntegration: SystemMenuIntegration | null = null;
 
   constructor(metadata: ExtensionMetadata) {
     super(metadata);
   }
 
   enable() {
-    this.manager = Gio.DBusProxy.makeProxyWrapper(ManagerInterface);
-    if (!panel.statusArea.quickSettings._system) {
-      this.queueModifySystemItem();
-    } else {
-      this.modifySystemItem();
+    try {
+      this.rebootManager = new SystemRebootManager();
+      this.menuIntegration = new SystemMenuIntegration(this.rebootManager);
+      this.menuIntegration.initialize();
+    } catch (error) {
+      log('Failed to enable extension:' + error);
+      this.disable();
     }
   }
 
   disable() {
-    this.manager = null;
-    this.rebootToBiosPowerMenuItem?.destroy();
-    this.rebootToBiosPowerMenuItem = null;
-    this.rebootToWindowsPowerMenuItem?.destroy();
-    this.rebootToWindowsPowerMenuItem = null;
-    this.proxy = null;
-    if (this.sourceId) {
-      GLib.Source.remove(this.sourceId);
-      this.sourceId = null;
+    try {
+      this.menuIntegration?.destroy();
+    } catch (error) {
+      log('Error during extension cleanup:' + error);
+    } finally {
+      this.menuIntegration = null;
+      this.rebootManager = null;
     }
-  }
-
-  private modifySystemItem() {
-    if (this.manager === null) return;
-    
-    this.menu = panel.statusArea.quickSettings._system?.quickSettingsItems[0].menu;
-    this.rebootToBiosPowerMenuItem = new RebootMenuItem('Restart to BIOS', () => this.reboot('auto-reboot-to-firmware-setup'));
-    this.rebootToWindowsPowerMenuItem = new RebootMenuItem('Restart to Windows', () => this.reboot('windows-11'));
-
-    this.proxy = this.manager(
-      Gio.DBus.system,
-      'org.freedesktop.login1',
-      '/org/freedesktop/login1',
-    );
-
-    this.menu!.addMenuItem(this.rebootToWindowsPowerMenuItem, 2);
-    this.menu!.addMenuItem(this.rebootToBiosPowerMenuItem, 3);
-  }
-
-  private queueModifySystemItem(): void {
-    this.sourceId = GLib.idle_add(GLib.PRIORITY_DEFAULT, () => {
-      if (!panel.statusArea.quickSettings._system) return GLib.SOURCE_CONTINUE;
-
-      this.modifySystemItem();
-      return GLib.SOURCE_REMOVE;
-    });
-  }
-
-  private async reboot(bootloaderTarget: string) {
-    const [, argv] = GLib.shell_parse_argv(`pkexec systemctl reboot --boot-loader-entry=${bootloaderTarget}`);
-    const proc = Gio.Subprocess.new(argv!, Gio.SubprocessFlags.NONE);
-    await proc.wait_check_async(null);
-    this.proxy?.RebootRemote(true);
   }
 }
